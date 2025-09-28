1f0dd3752e299badbe036f29556b74d5d3271f72
hasanahmad972 <ahmadhasaniec@gmail.com>
2025-09-28 18:28:22 +0530

    first Commit

---
+++ src/main/java/in/sp/main/SpringBootProject111Application.java
@@ -0,0 +1,13 @@
+package in.sp.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProject111Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootProject111Application.class, args);
+	}
+
+}
